f22cc4153dc1620ec5a3121f05c61e2a0bbb6b54
pilip-123 <pilipyom985@gmail.com>
2026-07-09 14:42:47 +0700

    Configure Gmail SMTP and fix reset link for customer users

--- E-Commerce_backend/app/Notifications/ResetPassword.php
+++ E-Commerce_backend/app/Notifications/ResetPassword.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -26,7 +27,12 @@ class ResetPassword extends Notification
 
     public function toMail(object $notifiable): MailMessage
     {
-        $url = route('password.reset', ['token' => $this->token, 'email' => $this->email]);
+        if ($notifiable instanceof User && $notifiable->role === User::ROLE_CUSTOMER) {
+            $frontendUrl = config('app.frontend_url', 'http://localhost:5173');
+            $url = $frontendUrl . '/#/reset-password?token=' . $this->token . '&email=' . urlencode($this->email);
+        } else {
+            $url = route('password.reset', ['token' => $this->token, 'email' => $this->email]);
+        }
 
         return (new MailMessage)
             ->subject('Reset Your Password')
